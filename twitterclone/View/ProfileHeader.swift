d9367f03ff0ef0f48073f02b983b74ab704c8993
Neezar <rforlyffe@gmaicl.com>
2021-04-22 17:22:35 +0800

    add search user and follow/unfollow feature

--- twitterclone/View/ProfileHeader.swift
+++ twitterclone/View/ProfileHeader.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol ProfileHeaderDelegate:class {
     func handleDismissal()
+    func handleEditProfileFollow (_ header: ProfileHeader)
 }
 
 class ProfileHeader: UICollectionReusableView {
@@ -97,7 +98,7 @@ class ProfileHeader: UICollectionReusableView {
         return view
     }()
     
-    private lazy var editProfileFollowButton: UIButton = {
+     lazy var editProfileFollowButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Follow", for: .normal)
         button.layer.borderColor = UIColor.twitterBlue.cgColor
@@ -165,7 +166,7 @@ class ProfileHeader: UICollectionReusableView {
     }
     
     @objc func handleEditProfileFollow() {
-        
+        delegate?.handleEditProfileFollow(self)
     }
     
     @objc func handleFollowersTapped() {
